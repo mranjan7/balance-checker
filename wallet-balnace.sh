ddf331ff8445edabaf9999fb455be73720edc615
Mukesh Ranjan <mukeshr@tuta.com>
2025-12-17 06:20:14 +0000

    latest commit

--- wallet-balnace.sh
+++ wallet-balnace.sh
@@ -6,7 +6,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No clor
+NC='\033[0m' # No color
 
 RPC_URL="https://api.mainnet-beta.solana.com"
 
@@ -86,7 +86,6 @@ display_balnace(){
 		done
 	fi
 	echo ""
-
 }
 
 WALLET=""
